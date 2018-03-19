8b0b0ad82970e74606cfb2409e5d5c603c8e9aa7
Francis <francispham89@gmail.com>
2018-03-19 16:54:03 -0700

    Finished Auth flow

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,21 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  // user.id is from MongoDB not from Google Auth
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    });
+});
 
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
@@ -8,7 +23,20 @@ passport.use(new GoogleStrategy({
   callbackURL: '/auth/google/callback'
 },
 (accessToken, refreshToken, profile, done) => {
-  console.log('access token',accessToken);
-  console.log('refresh token', refreshToken);
-  console.log('profile', profile);
+  // console.log('success callback')
+  // console.log('profile')
+  // console.log(profile);
+  // console.log('------------')
+  User.findOne ({ googleId: profile.id })
+    .then ((existingUser) => {
+      if (existingUser) {
+        done(null, existingUser);
+      } else {
+        new User ({ googleId: profile.id })
+          .save()
+          .then(user => done(null, user))
+
+      }
+    })
+
 }));
