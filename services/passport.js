d076ea2cf32258ba9526734da827ebe70dbe88d8
Francis <francispham89@gmail.com>
2018-03-19 22:01:20 -0700

    Install Redux and Auth Reducer

--- services/passport.js
+++ services/passport.js
@@ -11,10 +11,9 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-  User.findById(id)
-    .then(user => {
-      done(null, user);
-    });
+  User.findById(id).then(user => {
+    done(null, user);
+  });
 });
 
 passport.use(new GoogleStrategy({
@@ -22,22 +21,18 @@ passport.use(new GoogleStrategy({
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback',
   proxy: true
-},
-(accessToken, refreshToken, profile, done) => {
-  // console.log('success callback')
-  // console.log('profile')
-  // console.log(profile);
-  // console.log('------------')
-  User.findOne ({ googleId: profile.id })
-    .then ((existingUser) => {
+  },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({
+        googleId: profile.id
+      });
       if (existingUser) {
-        done(null, existingUser);
-      } else {
-        new User ({ googleId: profile.id })
-          .save()
-          .then(user => done(null, user))
-
+        return done(null, existingUser);
       }
-    })
-
-}));
+      const user = await new User({
+        googleId: profile.id
+      }).save();
+      done(null, user);
+    }
+  )
+);
