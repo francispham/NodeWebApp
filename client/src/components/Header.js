aa7a15940acd098595ffe1e33fdb86c78df9a0dd
Francis <francispham89@gmail.com>
2018-03-20 23:42:23 -0700

    Use Link tag for Header Logo

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   renderContent() {
@@ -16,13 +17,15 @@ class Header extends Component {
   }
 
   render () {
-      console.log(this.props);
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">
+          <Link
+            to={this.props.auth ? '/surveys' : '/'}
+            className="left brand-logo"
+          >
             Stmedi Spa
-          </a>
+          </Link>
           <ul className="right">
             {this.renderContent()}
           </ul>
---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>
+        The Home Page of Stmedi Spa
+      </h1>
+      Collect feedback form your users
+    </div>
+  );
+};
+
+export default Landing;
