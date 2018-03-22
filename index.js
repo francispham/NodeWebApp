13e0244b794f6e7c84ccd5a6db254f38d40e1cd2
Francis <francispham89@gmail.com>
2018-03-21 21:35:19 -0700

    Editing for Deploying

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ app.use(passport.session());
 // ROUTES
 require('./routes/authRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.statics('client/build'));
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  app.get('*', (req, res) => {
+    res.sendFile(path.resove(_dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
