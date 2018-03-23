eb7cf0b7ce2327d3de12a94cc4e019ffd80a1ba9
Francis <francispham89@gmail.com>
2018-03-23 11:51:54 -0700

    Setup ClientField component

--- client/src/components/clients/ClientForm.js
+++ client/src/components/clients/ClientForm.js
@@ -1,17 +1,30 @@
 // ClientForm shows a form for a user to add input
-import React, { Component } from 'react';
-import { reduxForm } from 'redux-form'
+import React, {Component} from 'react';
+import {reduxForm, Field} from 'redux-form';
+import ClientField from './ClientField';
 
 class ClientForm extends Component {
-  render() {
+
+  renderFields()  {
     return (
       <div>
-        ClientForm!
+        <Field type="text" name="Name" component={ClientField}/>
       </div>
     );
   }
+
+  render() {
+    return (<div>
+      <form onSubmit={
+        this.props.handleSubmit(
+          values => console.log(values)
+        )
+      }>
+        {this.renderFields()}
+        <button type="submit">Sumbit</button>
+      </form>
+    </div>);
+  }
 }
 
-export default reduxForm({
-  form: 'clientForm'
-}) (ClientForm);
+export default reduxForm({form: 'clientForm'})(ClientForm);
