6479123178ccb408c9af3c5120200c9d99a227f3
Francis <francispham89@gmail.com>
2018-03-22 20:23:33 -0700

    add some styling

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -12,7 +12,7 @@ class Payments extends Component {
         token={token => this.props.handleToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
       >
-        <button className="btn">
+        <button className="btn" style={{ 'background-color': 'maroon' }}>
           Add Credits
         </button>
       </StripeCheckout>
